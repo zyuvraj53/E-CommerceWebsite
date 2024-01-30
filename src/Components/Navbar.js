62d139b9907a9072e10a4c99f833757ffc20195f
zyuvraj53 <zyuvraj53@gmail.com>
2024-01-30 22:22:55 +0530

    Modal

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,68 @@
 import logo from "../logo.svg";
+import { useState } from "react";
+import OrderFulfillmentModal from "./OrderFulfillmentModal";
 
 export default function Navbar() {
+  const [openOrderFulfillmentModal, setOpenOrderFulfillmentModal] =
+    useState(false);
+  const [openStockTrackingModal, setStockTrackingModal] =
+    useState(false);
+
   return (
     <nav className="nav bg-[#333] flex text-white justify-between px-4 py-0">
       <div className="gap-8">
-      <ul className="flex justify-between p-0 m-0 list-none gap-4">
-        <li>
-          <a href="/">
-            <img src={logo} className="py-1" alt="logo" height={25} width={25} />
-          </a>
-        </li>
-        <li>
-          <a href="/" className="site-title text-2xl">
-            Site Title
-          </a>
-        </li>
-      </ul>
+        <ul className="flex justify-between p-0 m-0 list-none gap-4">
+          <li>
+            <a href="/">
+              <img
+                src={logo}
+                className="py-1"
+                alt="logo"
+                height={25}
+                width={25}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="/" className="site-title text-2xl">
+              Site Title
+            </a>
+          </li>
+        </ul>
       </div>
       <div className="text-inherit gap-8">
-      <ul className="flex justify-between p-0 m-0 list-none gap-4">
-        <li>
-          <button href="/OrderFulfillment">Order Fulfillment</button>
-        </li>
-        <li>
-          <a href="/StockTracking">Stock Tracking</a>
-        </li>
-      </ul>
+        <ul className="flex justify-between p-0 m-0 list-none gap-4">
+          <li>
+            <button
+              className="OrderFulfillModal"
+              onClick={() => {
+                setOpenOrderFulfillmentModal(true);
+              }}
+            >
+              Order Fulfillment
+            </button>
+            {openOrderFulfillmentModal && (
+              <OrderFulfillmentModal
+                closeModal={setOpenOrderFulfillmentModal}
+              />
+            )}
+          </li>
+          <li>
+            <button
+              className="StockTrackingModal"
+              onClick={() => {
+                setStockTrackingModal(true);
+              }}
+            >
+              Stock Tracking
+            </button>
+            {openStockTrackingModal && (
+              <OrderFulfillmentModal
+                closeModal={setStockTrackingModal}
+              />
+            )}
+          </li>
+        </ul>
       </div>
     </nav>
   );
---
+++ src/Components/OrderFulfillmentModal.js
@@ -0,0 +1,18 @@
+export default function StockTrackingModal({closeModal}) {
+  return (
+    <div className="modalBackground w-screen h-screen bg-[#C8C8C8] fixed justify-center items-center">
+      <div className="modalContainter w-96 h-96 rounded-xl bg-white flex flex-col p-6">
+        <button onClick={() => closeModal(false)}> X </button>
+        <div className="title">
+          <h1>Order Fulfilment</h1>
+        </div>
+        <div className="body">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="footer">
+          <button onClick={() => closeModal(false)}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+}
