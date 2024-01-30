f76dbc885a4ce068f9cc75fb57419236bf808f86
Yuvraj Singh <77329747+zyuvraj53@users.noreply.github.com>
2024-01-30 22:31:02 +0530

    Update OrderFulfillmentModal.js

--- src/Components/OrderFulfillmentModal.js
+++ src/Components/OrderFulfillmentModal.js
@@ -1,7 +1,7 @@
 export default function StockTrackingModal({closeModal}) {
   return (
     <div className="modalBackground w-screen h-screen bg-[#C8C8C8] fixed justify-center items-center">
-      <div className="modalContainter w-96 h-96 rounded-xl bg-white flex flex-col p-6">
+      <div className="modalContainter">
         <button onClick={() => closeModal(false)}> X </button>
         <div className="title">
           <h1>Order Fulfilment</h1>
